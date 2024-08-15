3d1a9d5d14c29ead5e5db1727913010fd253ba34
Ibrahim <mohu050302@gmail.com>
2024-08-15 17:33:10 +0530

    first commit

---
+++ android/app/src/main/java/com/saveetha/e_book/UserDashboardActivity.java
@@ -0,0 +1,23 @@
+package com.saveetha.e_book;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.saveetha.e_book.databinding.ActivityUserDashboardBinding;
+
+public class UserDashboardActivity extends AppCompatActivity {
+
+    ActivityUserDashboardBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityUserDashboardBinding.inflate(getLayoutInflater())
+        setContentView(binding.getRoot());
+    }
+}
