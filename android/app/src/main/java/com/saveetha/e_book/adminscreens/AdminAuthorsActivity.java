784d533a20347c13b09f84e888634aec9ad1ca53
TechieVivek1 <vivek13122000@gmail.com>
2024-08-17 22:38:24 +0530

    vivek changes

--- android/app/src/main/java/com/saveetha/e_book/adminscreens/AdminAuthorsActivity.java
+++ android/app/src/main/java/com/saveetha/e_book/adminscreens/AdminAuthorsActivity.java
@@ -7,16 +7,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.saveetha.e_book.R;
+import com.saveetha.e_book.adminscreens.adminadapters.AdminAuthorsAdapter;
+import com.saveetha.e_book.adminscreens.adminmodules.AdminAuthorsModule;
+import com.saveetha.e_book.databinding.ActivityAdminAuthorsBinding;
+
+import java.util.ArrayList;
 
 public class AdminAuthorsActivity extends AppCompatActivity {
+
+    ActivityAdminAuthorsBinding binding;
+    ArrayList<AdminAuthorsModule> authors = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_admin_authors);
 
+        binding = ActivityAdminAuthorsBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        loadAuthors();
+
+    }
+
+    private void loadAuthors() {
+
+        authors.add(new AdminAuthorsModule("Author 1","v@gmail.com","1234567890","https://imgs.search.brave.com/iWnCT3lR5iw046Q5mH0h4GoxTmSX4Zv0Bz7zgKuALfk/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA4LzI4LzU2LzA1/LzM2MF9GXzgyODU2/MDU1Nl9oZmdqczE2/VURONFZGSmNSVERq/S1N4cnNxSHQzQVdz/My5qcGc"));
+        authors.add(new AdminAuthorsModule("Author 2","v@gmail.com","1234567890","https://imgs.search.brave.com/iWnCT3lR5iw046Q5mH0h4GoxTmSX4Zv0Bz7zgKuALfk/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA4LzI4LzU2LzA1/LzM2MF9GXzgyODU2/MDU1Nl9oZmdqczE2/VURONFZGSmNSVERq/S1N4cnNxSHQzQVdz/My5qcGc"));
+        authors.add(new AdminAuthorsModule("Author 3","v@gmail.com","1234567890","https://imgs.search.brave.com/iWnCT3lR5iw046Q5mH0h4GoxTmSX4Zv0Bz7zgKuALfk/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA4LzI4LzU2LzA1/LzM2MF9GXzgyODU2/MDU1Nl9oZmdqczE2/VURONFZGSmNSVERq/S1N4cnNxSHQzQVdz/My5qcGc"));
+        authors.add(new AdminAuthorsModule("Author 4","v@gmail.com","1234567890","https://imgs.search.brave.com/iWnCT3lR5iw046Q5mH0h4GoxTmSX4Zv0Bz7zgKuALfk/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA4LzI4LzU2LzA1/LzM2MF9GXzgyODU2/MDU1Nl9oZmdqczE2/VURONFZGSmNSVERq/S1N4cnNxSHQzQVdz/My5qcGc"));
+
+        AdminAuthorsAdapter adapter = new AdminAuthorsAdapter(authors,this);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerView.setAdapter(adapter);
     }
 }
--- android/app/src/main/java/com/saveetha/e_book/adminscreens/AdminReviewedActivity.java
+++ android/app/src/main/java/com/saveetha/e_book/adminscreens/AdminReviewedActivity.java
@@ -1,5 +1,6 @@
 package com.saveetha.e_book.adminscreens;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,16 +8,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.saveetha.e_book.R;
+import com.saveetha.e_book.adminscreens.adminadapters.AdminManageBooksAdapter;
+import com.saveetha.e_book.adminscreens.adminmodules.AdminBooksModule;
+import com.saveetha.e_book.databinding.ActivityAdminReviewedBinding;
+
+import java.util.ArrayList;
 
 public class AdminReviewedActivity extends AppCompatActivity {
+
+    ActivityAdminReviewedBinding binding;
+    ArrayList<AdminBooksModule> adminBooksModuleList;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_admin_reviewed);
 
+        binding = ActivityAdminReviewedBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        loadReviewedBooks();
+
+    }
+
+    private void loadReviewedBooks() {
+        adminBooksModuleList = new ArrayList<>();
+
+        adminBooksModuleList.add(new AdminBooksModule("Anesthesia","https://imgs.search.brave.com/C9Ux4Cy7nVBYvAG10ljDU-DRtkCdEJP-peea-8KXB3g/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzlkL2Rh/L2M3LzlkZGFjNzRl/MDhkMTU1MWYwNTNi/ZDBjNzI1YjQwNzFj/LmpwZw","This text serves as a placeholder, offering a glimpse of content in design layouts. It helps visualize the arrangement and appearance of text elements within a project. While the words may be generic, the structure and format reflect how actual content will look. Use this sample to fine-tune spacing, alignment, and overall visual impact before finalizing the real text.","200"));
+        adminBooksModuleList.add(new AdminBooksModule("pharmacist","https://imgs.search.brave.com/C9Ux4Cy7nVBYvAG10ljDU-DRtkCdEJP-peea-8KXB3g/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzlkL2Rh/L2M3LzlkZGFjNzRl/MDhkMTU1MWYwNTNi/ZDBjNzI1YjQwNzFj/LmpwZw","This text serves as a placeholder, offering a glimpse of content in design layouts. It helps visualize the arrangement and appearance of text elements within a project. While the words may be generic, the structure and format reflect how actual content will look. Use this sample to fine-tune spacing, alignment, and overall visual impact before finalizing the real text.","400"));
+        adminBooksModuleList.add(new AdminBooksModule("Pharmaceutics","https://imgs.search.brave.com/C9Ux4Cy7nVBYvAG10ljDU-DRtkCdEJP-peea-8KXB3g/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzlkL2Rh/L2M3LzlkZGFjNzRl/MDhkMTU1MWYwNTNi/ZDBjNzI1YjQwNzFj/LmpwZw","This text serves as a placeholder, offering a glimpse of content in design layouts. It helps visualize the arrangement and appearance of text elements within a project. While the words may be generic, the structure and format reflect how actual content will look. Use this sample to fine-tune spacing, alignment, and overall visual impact before finalizing the real text.","600"));
+        adminBooksModuleList.add(new AdminBooksModule("Anesthesia","https://imgs.search.brave.com/C9Ux4Cy7nVBYvAG10ljDU-DRtkCdEJP-peea-8KXB3g/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzlkL2Rh/L2M3LzlkZGFjNzRl/MDhkMTU1MWYwNTNi/ZDBjNzI1YjQwNzFj/LmpwZw","This text serves as a placeholder, offering a glimpse of content in design layouts. It helps visualize the arrangement and appearance of text elements within a project. While the words may be generic, the structure and format reflect how actual content will look. Use this sample to fine-tune spacing, alignment, and overall visual impact before finalizing the real text.","4000"));
+
+        AdminManageBooksAdapter adapter = new AdminManageBooksAdapter(adminBooksModuleList,context);
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        binding.recyclerView.setAdapter(adapter);
     }
 }
