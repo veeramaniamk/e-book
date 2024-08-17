58739bfdf418d4d39a9c9520e246c819cbc15649
TechieVivek1 <vivek13122000@gmail.com>
2024-08-17 20:07:29 +0530

    added admin views

--- android/app/src/main/java/com/saveetha/e_book/adminscreens/AdminRequestFragment.java
+++ android/app/src/main/java/com/saveetha/e_book/adminscreens/AdminRequestFragment.java
@@ -1,23 +1,60 @@
 package com.saveetha.e_book.adminscreens;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.saveetha.e_book.R;
+import com.saveetha.e_book.databinding.FragmentAdminRequestBinding;
 
 
 public class AdminRequestFragment extends Fragment {
 
+    FragmentAdminRequestBinding binding;
+
+    FragmentActivity activity;
+    Context context;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_request, container, false);
+
+        binding = FragmentAdminRequestBinding.inflate(inflater, container, false);
+
+        try {
+            activity = getActivity();
+            context = getContext();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        onClickAction();
+
+
+
+        return binding.getRoot();
+    }
+
+    private void onClickAction() {
+
+        binding.card1.setOnClickListener(v -> {
+            startActivity(new Intent(context,AdminReviewedActivity.class));
+        });
+
+        binding.card2.setOnClickListener(v -> {
+            startActivity(new Intent(context,AdminPurchasedActivity.class));
+        });
+
+        binding.card3.setOnClickListener(v -> {
+            startActivity(new Intent(context,AdminAuthorsActivity.class));
+        });
+
     }
 }
---
+++ android/app/src/main/java/com/saveetha/e_book/adminscreens/AdminReviewedActivity.java
@@ -0,0 +1,22 @@
+package com.saveetha.e_book.adminscreens;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.saveetha.e_book.R;
+
+public class AdminReviewedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_admin_reviewed);
+
+    }
+}
