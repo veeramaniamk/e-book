ad1634d2db287ae14b1e9232924b4d01019cc417
Veeramani A <168632267+veeramaniamk@users.noreply.github.com>
2024-08-18 10:35:05 +0530

    Update getAllPurchesedBooks.js

--- backend/src/admin/getAllPurchesedBooks.js
+++ backend/src/admin/getAllPurchesedBooks.js
@@ -1,11 +1,17 @@
 const mysql = require("../database/connection");
 
-const getAllReviews = (req, res) => {
+const getAllPurchesedBooks = (req, res) => {
 
+    const SITE_COVER_IMAGE_URL = req.protocol + '://' + req.get('host') + '/cover_image/';
+    const SITE_BOOK_URL        = req.protocol + '://' + req.get('host') + '/books/';
+    const SITE_DEMO_BOOK_URL   = req.protocol + '://' + req.get('host') + '/demo_page/';
+    const SITE_URL             = req.protocol + '://' + req.get('host') + '/asserts/';
 
-    const SITE_URL = req.protocol + '://' + req.get('host') + '/asserts/';
-
-    let query = `SELECT review.id as review_id, signup.id as signup_id, review_text, user_id, publisher_id, book_id, date, name, phone, gender, user_type, email, profile_photo  FROM review LEFT JOIN signup ON review.user_id = signup.id`;
+    let query = `select pb.id , pb.date as purchased_book_date, pb.transaction_id as purchased_book_transaction_id, 
+    user_id, book_id, pb.publisher_id, pb.price as user_payed_amount, eb.publisher_name as eb_publisher_name, 
+    book_titile, book_cover_image, book_pdf, demo_file, category_name, auther_name, year_of_the_book, eb.price as book_price,
+     eb.book_submit_date, su.name as user_name, su.phone as user_phone, su.user_type, email, 
+     profile_photo from purchased_books pb inner join e_books eb on pb.book_id = eb.id inner join signup su on su.id = pb.user_id`;
     
    mysql.query(query, (err, result) => {
        if(err) {
@@ -18,31 +24,38 @@ const getAllReviews = (req, res) => {
            return res.status(400).send({status:400, message:"Not Found"});
        }
 
-       const data = [];
+       const sendData = [];
 
-       result.forEach(userInfo => {
-                const user     = {};
+       result.forEach(element => {
+           const obj                    = {};
+           obj.purches_id               = element.id;
+           obj.purches_date             = element.purchased_book_date;
+           obj.purchased_transaction_id = element.purchased_book_transaction_id;
+           obj.user_id                  = element.user_id;
+           obj.book_id                  = element.book_id;
+           obj.publisher_id             = element.publisher_id;
+           obj.user_payed_amount        = element.user_payed_amount;
+           obj.book_publisher_name      = element.eb_publisher_name
+           obj.book_titile              = element.book_titile;
+           obj.book_cover_image         = SITE_COVER_IMAGE_URL + element.book_cover_image;
+           obj.book_pdf                 = SITE_BOOK_URL + element.book_pdf;
+           obj.demo_book                = SITE_DEMO_BOOK_URL + element.demo_file;
+           obj.category_name            = element.category_name;
+           obj.book_auther_name         = element.auther_name;
+           obj.year                     = element.year_of_the_book;
+           obj.book_price               = element.book_price;
+           obj.user_name                = element.user_name;
+           obj.user_phone               = element.user_phone;
+           obj.user_type                = element.user_type;
+           obj.email                    = element.email;
+           obj.user_profile_photo       = SITE_URL + element.profile_photo;
+           sendData.push(obj);
+       });
 
-                user.review_id = userInfo.review_id;
-                user.user_id   = userInfo.signup_id;
-                user.book_id   = userInfo.book_id;
-                user.publisher_id = userInfo.publisher_id;
-                user.name      = userInfo.name;
-                user.phone     = userInfo.phone;
-                user.email     = userInfo.email;
-                user.user_type = userInfo.user_type;
-                user.gender    = userInfo.gender;
-                user.review_text = userInfo.review_text;
-                user.date      = userInfo.date;
-                user.profile   = SITE_URL + userInfo.profile_photo;
-                data.push(user);
-                   
-           });
-
-        return res.status(200).send({ status:200, message:'Success', data:data });
+        return res.status(200).send({ status:200, message:'Success', data:sendData });
        
    })
 
 }
 
-module.exports = {getAllReviews}
+module.exports = { getAllPurchesedBooks };
