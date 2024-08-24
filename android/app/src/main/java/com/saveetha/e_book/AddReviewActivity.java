7f6b4332947b4c297817556e6662ff99ec6104ae
TechieVivek1 <vivek13122000@gmail.com>
2024-08-24 22:46:26 +0530

    added user modules

--- android/app/src/main/java/com/saveetha/e_book/AddReviewActivity.java
+++ android/app/src/main/java/com/saveetha/e_book/AddReviewActivity.java
@@ -1,11 +1,22 @@
 package com.saveetha.e_book;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.saveetha.e_book.databinding.ActivityAddReviewBinding;
+import com.saveetha.e_book.reviewerscrees.ReviewerAddBookActivity;
+import com.saveetha.e_book.reviewerscrees.ReviewerAddCategoryActivity;
 
 public class AddReviewActivity extends AppCompatActivity {
 
@@ -26,12 +37,38 @@ public class AddReviewActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                //TODO
-                // add review
-                // show Dialog Box To Add Review
-                // dialog Box @layout Name : add_review_layout.xml
 
             }
         });
+    }
+
+    private void showPopupWindow(View anchorView) {
+        // Inflate the custom layout
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.add_review_layout, null);
+
+        // Create the PopupWindow
+        PopupWindow popupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) EditText review = popupView.findViewById(R.id.reviewET);
+        Button send = popupView.findViewById(R.id.sendBtn);
+
+
+
+        send.setOnClickListener(v -> {
+            String reviewText = review.getText().toString();
+            if (reviewText.isEmpty()) {
+                Toast.makeText(this, "write something!", Toast.LENGTH_SHORT).show();
+            } else {
+                sendReview();
+            }
+        });
+
+        popupWindow.showAtLocation(anchorView, Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM, 0, 0); // Adjust gravity and position as needed
+
+    }
+
+    private void sendReview() {
+
     }
 }
